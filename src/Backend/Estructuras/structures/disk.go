d4ce67bbb8d53fbfc53fdcffba8020721b03db08
Oscar <3055716560301@ingenieria.usac.edu.gt>
2025-12-29 01:53:18 -0600

    feat
    cambios minimos

--- src/Backend/Estructuras/structures/disk.go
+++ src/Backend/Estructuras/structures/disk.go
@@ -1,24 +1,16 @@
 package estructures
 
-import "time"
-
 // ESTRUCTURAS DISCOS
 
-type MBR struct { //153bytes
-	Mbr_tamano         int32        //tamaño total del disco en bytes
-	Mbr_fecha_creacion time.Time    //fecha y hora de creacion del disco (time)
-	Mbr_disk_signature int32        //numero random que identifica de forma unica cada disco
-	Dsk_fit            byte         //tipo de ajuste de particion B (best) F (first) W (worst)
-	Mbr_partitions     [4]Partition //estructura con informacion de las 4 particiones
+type EBR struct { //30 bytes
+	Part_mount int8     //indica si esta montada o no
+	Part_fit   byte     //tipo de ajuste B (best) F (first) W (worst)
+	Part_start int32    //indica en que byte del disco inicia particion
+	Part_s     int32    //contiene tamaño total de particion en bytes
+	Part_next  int32    //byte en el que esta proximo EBR -1 si no hay siguiente
+	Name       [16]byte //nombre de particion
 }
 
-type MBR_Obtener struct {
-	Disco          string
-	Disco_Path     string
-	Mbr_partitions [4]Particion_Enviar
-}
-
-//-------- PARTICIONES --------
 type Partition struct { //38 bytes
 	Part_status      int8     //indica si particion esta montada o no
 	Part_type        byte     //indica el tipo de particion P (primaria) E (extendida)
@@ -30,6 +22,14 @@ type Partition struct { //38 bytes
 	Part_id          [4]byte  //indica el id de particion generada al montar la misma
 }
 
+type MBR struct { //153bytes
+	Mbr_tamano         int32        //tamaño total del disco en bytes
+	Mbr_fecha_creacion int32        //fecha y hora de creacion del disco (time)
+	Mbr_disk_signature int32        //numero random que identifica de forma unica cada disco
+	Dsk_fit            byte         //tipo de ajuste de particion B (best) F (first) W (worst)
+	Mbr_partitions     [4]Partition //estructura con informacion de las 4 particiones
+}
+
 type Particion_Enviar struct {
 	Particion  string
 	Type       string
@@ -37,12 +37,8 @@ type Particion_Enviar struct {
 	Id_mounted string
 }
 
-//-------- EXTENDED BOOT RECORD --------
-type EBR struct { //30 bytes
-	Part_mount int8     //indica si esta montada o no
-	Part_fit   byte     //tipo de ajuste B (best) F (first) W (worst)
-	Part_start int32    //indica en que byte del disco inicia particion
-	Part_s     int32    //contiene tamaño total de particion en bytes
-	Part_next  int32    //byte en el que esta proximo EBR -1 si no hay siguiente
-	Name       [16]byte //nombre de particion
+type MBR_Obtener struct {
+	Disco          string
+	Disco_Path     string
+	Mbr_partitions [4]Particion_Enviar
 }
